9cb0a1949836a3c2f9a1f740d51fd3c845dd23f2
alvro22 <alvaro.olivaressanchez@riberadeltajo.es>
2025-04-23 19:07:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
     const header = document.querySelector(".header"); // Seleccionamos el header
-    if (window.scrollY > 50) { // Si el usuario ha bajado más de 50px
+    if (window.scrollY > 0) { // Si el usuario ha bajado más de 50px
         header.classList.add("scrolled"); // Agregamos la clase
     } else {
         header.classList.remove("scrolled"); // Quitamos la clase si vuelve arriba
