3bf7ea2781750e0f196812aa24969f970eaa5928
alvro22 <alvaro.olivaressanchez@riberadeltajo.es>
2025-04-23 19:08:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", function () {
 
 window.addEventListener("scroll", function() {
     let imagen = document.getElementById("menuImage");
-    if (window.scrollY > 50) { // Si el scroll supera los 50px
+    if (window.scrollY > 0) { // Si el scroll supera los 50px
         imagen.src = "./assets/images/logo_grupocga.png"; // Cambia la imagen
     } else {
         imagen.src = "./assets/images/logo_grupocga_blanco.png"; // Vuelve a la imagen original
